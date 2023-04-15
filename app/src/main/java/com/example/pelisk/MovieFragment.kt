9bc0261925706a4d6c9e474c562e611ed9b42abf
Deskin Velasquez <dedosvela@gmail.com>
2023-04-15 11:05:48 -0500

    Implementacion de ViewBinding

--- app/src/main/java/com/example/pelisk/MovieFragment.kt
+++ app/src/main/java/com/example/pelisk/MovieFragment.kt
@@ -5,11 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.pelisk.databinding.FragmentMovieBinding
 
 class MovieFragment : Fragment(R.layout.fragment_movie) {
 
+    private lateinit var binding:FragmentMovieBinding
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding = FragmentMovieBinding.bind(view)
+        binding.progressBar.visibility = View.GONE
     }
 
 
