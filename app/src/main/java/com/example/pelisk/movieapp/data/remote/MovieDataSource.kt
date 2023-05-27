707ba3d91bcb6e06b1f3b0e92d837aac5d678ffb
Deskin Velasquez <dedosvela@gmail.com>
2023-05-27 12:46:27 -0500

    Implementando la capa de repositorio

--- app/src/main/java/com/example/pelisk/movieapp/data/remote/MovieDataSource.kt
+++ app/src/main/java/com/example/pelisk/movieapp/data/remote/MovieDataSource.kt
@@ -6,6 +6,9 @@ import com.example.pelisk.movieapp.repository.WebService
 
 class MovieDataSource(private val webService: WebService) {
 
+    //suspend o corrutinas, una función suspend significa que hasta que no vaya hasta el servidor el
+    //flujo de codigo no continua, suspend en como un countdownlactn para las funciones
+
     suspend fun getUpcomingMovies(): MovieList = webService.getUpcomingMovies(AppConstans.API_KEY)
 
     suspend fun getTopRatedMovies(): MovieList = webService.getTopRatedMovies(AppConstans.API_KEY)
--- app/src/main/java/com/example/pelisk/movieapp/presentation/MovieViewModel.kt
+++ app/src/main/java/com/example/pelisk/movieapp/presentation/MovieViewModel.kt
@@ -1,4 +1,31 @@
 package com.example.pelisk.movieapp.presentation
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.liveData
+import com.example.pelisk.movieapp.core.Resource
+import com.example.pelisk.movieapp.repository.MovieRepository
+import kotlinx.coroutines.Dispatchers
+import okhttp3.Dispatcher
 
-class MovieViewModel {
+class MovieViewModel(private val repo: MovieRepository): ViewModel() {
+
+    //para usar las extensiones de liveData habrá que implemetar la libreria en el gradle.app
+    //el liveData va a ejecutar el hilo donde se llevará a cabo esa corrutina
+    fun fetchUpcomingMovies() = liveData(Dispatchers.IO) {
+        emit(Resource.Loading())
+
+        try {
+            emit(Resource.Success(repo.getUpcomingMovies()))
+        }catch (e: Exception) {
+
+        }
+
+    }
+
+}
+
+class MovieVewModelFactory(private val repo: MovieRepository): ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return modelClass.getConstructor(MovieRepository::class.java).newInstance(repo)
+    }
 }
